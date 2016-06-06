960008c42370ae8a39e4ad37951b6f8fc0456379
jota <jorgeoterodelrio@gmail.com>
2016-06-06 09:05:03 +0200

    Clase Prin(Main) refactorizada: nombre de objetos cambiados, eliminado throws de IOException y su respectivo import, cambiados mensajes con resultados, utilizados getters  y setters para acceder al valor de la suma y nombre de variables que  reciben el valor de la suma cambiados.

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -1,13 +1,16 @@
 package examenfinalparte1;
-import java.io.IOException;
+
 public class Prin {
-public static void main(String[] args) throws IOException{
-Principal obj = new Principal(10,20);
-int n = obj.i;
-System.out.println("Es: " + n);
-Principal nm = new Principal(10);
-int m = nm.i;
-System.out.println("Es: " + m);
-Principal mn = new Principal();
-int nn = mn.i;
-System.out.println("Es: " + nn);}}
+
+    public static void main(String[] args){
+        Suma suma1 = new Suma(10, 20);
+        int suma1_numero = suma1.getNumero();
+        System.out.println("La suma de 10 y 20 es: " + suma1_numero);
+        Suma suma2 = new Suma(10);
+        int suma2_numero = suma2.getNumero();
+        System.out.println("La suma de 10 y 10 es: " + suma2_numero);
+        Suma suma3 = new Suma();
+        int suma3_numero = suma3.getNumero();
+        System.out.println("La suma sin parámetros es: " + suma3_numero);
+    }
+}

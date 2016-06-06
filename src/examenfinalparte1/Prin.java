256f99447e4d6e619bb85489cf098557d2327db1
jota <jorgeoterodelrio@gmail.com>
2016-06-06 09:24:35 +0200

    Añadido JavaDoc y comentarios

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -1,16 +1,21 @@
 package examenfinalparte1;
 
+/**
+ * Clase para mostrar por pantalla los valores de la variable de objeto
+ * dependiendo de el constructor al que se llame.
+ * @author jota
+ */
 public class Prin {
 
     public static void main(String[] args){
         Suma suma1 = new Suma(10, 20);
-        int suma1_numero = suma1.getNumero();
+        int suma1_numero = suma1.getNumero(); //Suma 10+20
         System.out.println("La suma de 10 y 20 es: " + suma1_numero);
         Suma suma2 = new Suma(10);
-        int suma2_numero = suma2.getNumero();
+        int suma2_numero = suma2.getNumero(); //El doble de 10
         System.out.println("La suma de 10 y 10 es: " + suma2_numero);
         Suma suma3 = new Suma();
-        int suma3_numero = suma3.getNumero();
+        int suma3_numero = suma3.getNumero(); //Valdrá -1
         System.out.println("La suma sin parámetros es: " + suma3_numero);
     }
 }
--- src/examenfinalparte1/Suma.java
+++ src/examenfinalparte1/Suma.java
@@ -1,5 +1,9 @@
 package examenfinalparte1;
 
+/**
+ * Permite realizar sumas de dos números y duplicar un número
+ * @author jota
+ */
 public class Suma {
 
     private int numero;
@@ -11,19 +15,39 @@ public class Suma {
     public void setNumero(int numero) {
         this.numero = numero;
     }
-
+    
+    /**
+     * Realiza la suma de dos número enteros y se la asigna al atributo
+     * del objeto.
+     * @param n1 Uno de los dos operandos
+     * @param n2 El otro operando
+     */
     public Suma(int n1, int n2) {
         numero = sumar(n1, n2);
     }
-
+    
+    /**
+     * Realiza el duplo del número que recibe como parámetro y se lo asigna al 
+     * atributo del objeto.
+     * @param n Número entero del que se va a realizar el duplo
+     */
     public Suma(int n) {
         numero = sumar(n, n);
     }
-
+    
+    /**
+     * Constructor por defecto, el valor de el atributo de objeto será -1.
+     */
     public Suma() {
         numero = -1;
     }
-
+    
+    /**
+     * Realiza la suma de dos número enteros recibidos por parámetros.
+     * @param n1 Uno de los dos operandos
+     * @param n2 El otro operando
+     * @return La suma de los dos números
+     */
     private int sumar(int n1, int n2) {
         return n1 + n2;
     }
